33c42041ce75e016f6dc2efb1a6fa7c03c997ca8
Omii3663 <kolteomkar2003@gmail.com>
2026-07-02 18:25:28 +0530

    Complete Assignment 10

--- Assignment10/Q2.py
+++ Assignment10/Q2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def Sum(No):
     sum = 0
     for i in range(1,No+1):
@@ -16,4 +17,24 @@ def main():
     total = Sum(No)
 
 if __name__ == "__main__":
+=======
+def Sum(No):
+    sum = 0
+    for i in range(1,No+1):
+        sum  = sum + i
+
+    print(sum)   
+    
+def main():
+    print('-'*40)
+    print("------Sum Of N natural number ------")
+    print('-'*40)
+
+    print("Enter Number :")
+    No = int(input())
+    print(f"Sum of First  {No} Natural Number is :")
+    total = Sum(No)
+
+if __name__ == "__main__":
+>>>>>>> f0edf1d (Complete Assignment 10)
     main()
--- Assignment10/Q3.py
+++ Assignment10/Q3.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def Factorial(No):
     count = 1
     for i in range(1,No+1):
@@ -16,4 +17,24 @@ def main():
     total = Factorial(No)
 
 if __name__ == "__main__":
+=======
+def Factorial(No):
+    count = 1
+    for i in range(1,No+1):
+        count  = count *   i
+
+    print(count)   
+    
+def main():
+    print('-'*40)
+    print("------Factorial of Number ------")
+    print('-'*40)
+
+    print("Enter Number :")
+    No = int(input())
+    print(f"The Factorial of {No}  is :")
+    total = Factorial(No)
+
+if __name__ == "__main__":
+>>>>>>> f0edf1d (Complete Assignment 10)
     main()
