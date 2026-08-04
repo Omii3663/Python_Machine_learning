dd127d6b3f16b48975b4e8c83276360bac096552
Omii3663 <kolteomkar2003@gmail.com>
2026-08-04 15:14:12 +0530

    Assignment29_5

--- Assignment29/Assignment29_5.py
+++ Assignment29/Assignment29_5.py
@@ -1,12 +1,18 @@
+import os
 def Chkfrequency(fileName,find):
-    fobj=open(fileName,"r")
-    count=0
-    for i in fobj:
-        if find in i:
-            count=count+1
-    print(f"Letter {find} is present {count} times in the file {fileName}")
-    fobj.close()
-
+    try:
+        if os.path.exists(fileName):
+            fobj=open(fileName,"r")
+            count=0
+            for i in fobj:
+                if find in i:
+                    count=count+1
+            print(f"Letter {find} is present {count} times in the file {fileName}")
+            fobj.close()
+        else:
+            print(f"Error: The file '{fileName}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred while processing the file: {e}")
 def main():
     print("Enter File Name Which You Want To Search  : ")
     fileName=input()
